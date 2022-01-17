1ec8b792a5037e0e1f38f67b81aab20b189703b9
Yotam <yotam.mohel@gmail.com>
2022-01-17 16:06:26 +0200

    Added some SQL inserts.

--- SikumkumServer/Database Scripts/DBDataLoad.sql
+++ SikumkumServer/Database Scripts/DBDataLoad.sql
@@ -2,4 +2,8 @@ use DBSikumkum
 INSERT INTO Users(Username, Email, IsAdmin,NumUploads, Password, Rating)
 VALUES ('yotam', 'yotam@gmail.com', 0, 0, 'banana', 0);
 INSERT INTO SUBJECT(SubjectName) 
-VALUES ('Physics'), ('Math'), ('Chemistry'), ('History'),('Jewish Bible'),('English'),('Hebrew'),('Music')
+VALUES  ('מתמטיקה'),('אנגלית'), ('פיזיקה'), ('כימיה'), ('היסטוריה'),('תנ"ך'),('עברית'),('אזרחות')
+INSERT INTO FileTypes(TypeName) 
+VALUES ('סיכום'), ('מטלה'), ('תרגול')
+INSERT INTO StudyYear(YearName)
+VALUES ('תיכון'), ('חטיבה'), ('יסודי'), ('אוניברסיטה')
